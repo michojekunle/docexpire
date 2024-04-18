7b51720839964207627d8c180aa890b00ade94bd
AMD <michojekunle1@gmail.com>
2024-04-18 20:55:59 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,138 @@
+const { Menu } = require('electron');
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
+const url = require('url');
+
+let main;
+let addDocWin;
+
+const mainMenu = [
+  { label: '' },
+  { 
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add Document',
+        click() {
+          addDocumentWindow()
+        }
+      }, 
+      {
+        label: 'Clear documents'
+      }, 
+      // for cloud syncing for the future
+      {
+        label: 'User Details'
+      },
+      {
+        label: 'Exit App',
+        accelerator: process.platform == 'darwin' ? 'Command+E' : 'Ctrl+E',
+        click() {
+          app.quit()
+        }
+      }
+    ]
+  }
+]
+
+function addWindow(name, params) {
+  let win = new BrowserWindow(params)
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, `${name}.html`),
+    protocol: 'file',
+    slashes: true
+  }))
+
+  return win;
+}
+
+function menuMac () {
+  if(process.platform == 'darwin'){
+    mainMenu.unshift({label: ''})
+  }
+}
+
+function enableDevTools() {
+  if (process.env.NODE_ENV === undefined ||
+    process.env.NODE_ENV !== 'production') {
+    mainMenu.push({
+    label: 'DevTools',
+    submenu: [
+    {
+    label: 'Toggle DevTools',
+    accelerator: process.platform == 'darwin' ?
+    'Command+T' : 'Ctrl+T',
+    click(i, fw) {
+    fw.toggleDevTools();
+    }
+    }
+    ]
+    });
+    }   
+}
+
+function displayMenu () {
+  menuMac()
+
+  const menu = Menu.buildFromTemplate(mainMenu);
+
+  Menu.setApplicationMenu(menu);
+}
+
+
+function addDocumentWindow() {
+  addDocWin = addWindow('doc', {
+    width: 200,
+    height: 300,
+    title: 'Add Document'
+  });
+
+  // Garbage collection
+ addDocWin.on('close', () => {
+  addDocWin = null;
+  });
+ 
+}
+
+
+
+app.on('ready', () => {
+  // create browser window
+  main = addWindow('main', {});
+
+  displayMenu()
+
+  main.on('closed', () => {
+    app.quit();
+  });
+
+})
+
+// function createWindow () {
+//   const win = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       preload: path.join(__dirname, 'preload.js')
+//     }
+//   })
+
+//   win.loadFile('index.html')
+// }
+
+// app.whenReady().then(() => {
+//   createWindow()
+
+//   app.on('activate', () => {
+//     if (BrowserWindow.getAllWindows().length === 0) {
+//       createWindow()
+//     }
+//   })
+// })
+
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') {
+//     app.quit()
+//   }
+// })
