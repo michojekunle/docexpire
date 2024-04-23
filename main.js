0ce96d73ccab57f6b75bae003925031f5e899df4
AMD <michojekunle1@gmail.com>
2024-04-23 01:04:56 +0100

    changes

--- main.js
+++ main.js
@@ -1,113 +1,121 @@
-const { Menu } = require('electron');
-const { app, BrowserWindow } = require('electron/main')
-const path = require('node:path')
-const url = require('url');
+const { Menu, ipcMain } = require("electron");
+const { app, BrowserWindow } = require("electron/main");
+const path = require("node:path");
+const url = require("url");
 
 let main;
 let addDocWin;
 
 const mainMenu = [
-  { label: '' },
-  { 
-    label: 'File',
+  { label: "" },
+  {
+    label: "File",
     submenu: [
       {
-        label: 'Add Document',
+        label: "Add Document",
         click() {
-          addDocumentWindow()
-        }
-      }, 
-      {
-        label: 'Clear documents'
-      }, 
-      // for cloud syncing for the future
-      {
-        label: 'User Details'
+          addDocumentWindow();
+        },
       },
       {
-        label: 'Exit App',
-        accelerator: process.platform == 'darwin' ? 'Command+E' : 'Ctrl+E',
+        label: "Clear documents",
         click() {
-          app.quit()
-        }
-      }
-    ]
-  }
-]
+          main.webContents.send("doc:clear");
+        },
+      },
+      // for cloud syncing for the future
+      {
+        label: "User Details",
+      },
+      {
+        label: "Exit App",
+        accelerator: process.platform == "darwin" ? "Command+E" : "Ctrl+E",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
 
 function addWindow(name, params) {
-  let win = new BrowserWindow(params)
+  let win = new BrowserWindow(params);
 
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, `${name}.html`),
-    protocol: 'file',
-    slashes: true
-  }))
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `${name}.html`),
+      protocol: "file",
+      slashes: true,
+    })
+  );
 
   return win;
 }
 
-function menuMac () {
-  if(process.platform == 'darwin'){
-    mainMenu.unshift({label: ''})
+function menuMac() {
+  if (process.platform == "darwin") {
+    mainMenu.unshift({ label: "" });
   }
 }
 
 function enableDevTools() {
-  if (process.env.NODE_ENV === undefined ||
-    process.env.NODE_ENV !== 'production') {
+  if (
+    process.env.NODE_ENV === undefined ||
+    process.env.NODE_ENV !== "production"
+  ) {
     mainMenu.push({
-    label: 'DevTools',
-    submenu: [
-    {
-    label: 'Toggle DevTools',
-    accelerator: process.platform == 'darwin' ?
-    'Command+T' : 'Ctrl+T',
-    click(i, fw) {
-    fw.toggleDevTools();
-    }
-    }
-    ]
+      label: "DevTools",
+      submenu: [
+        {
+          label: "Toggle DevTools",
+          accelerator: process.platform == "darwin" ? "Command+T" : "Ctrl+T",
+          click(i, fw) {
+            fw.toggleDevTools();
+          },
+        },
+      ],
     });
-    }   
+  }
 }
 
-function displayMenu () {
-  menuMac()
+function displayMenu() {
+  menuMac();
+  enableDevTools();
 
   const menu = Menu.buildFromTemplate(mainMenu);
 
   Menu.setApplicationMenu(menu);
 }
 
-
 function addDocumentWindow() {
-  addDocWin = addWindow('doc', {
+  addDocWin = addWindow("doc", {
     width: 200,
     height: 300,
-    title: 'Add Document'
+    title: "Add Document",
   });
 
   // Garbage collection
- addDocWin.on('close', () => {
-  addDocWin = null;
+  addDocWin.on("close", () => {
+    addDocWin = null;
   });
- 
 }
 
+ipcMain.on("doc:add", (e, item) => {
+  console.log(item);
+  main.webContents.send("doc:add", item);
+  addDocWin.close();
+});
 
-
-app.on('ready', () => {
+app.on("ready", () => {
   // create browser window
-  main = addWindow('main', {});
+  main = addWindow("main", {});
 
-  displayMenu()
+  displayMenu();
 
-  main.on('closed', () => {
+  main.on("closed", () => {
     app.quit();
   });
-
-})
+});
 
 // function createWindow () {
 //   const win = new BrowserWindow({
